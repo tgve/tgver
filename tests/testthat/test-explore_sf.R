41489b4ec46c3106fcc84f55bab00be36e673147
Layik Hama <layik.hama@gmail.com>
2021-11-17 14:23:22 +0000

    tests

--- tests/testthat/test-explore_sf.R
+++ tests/testthat/test-explore_sf.R
@@ -2,7 +2,27 @@ test_that("explore_sf works", {
   expect_error(explore_sf())
   # TODO: install sf in tic.yml before uncommenting these
   # nc = sf::st_read(system.file("shape/nc.shp", package="sf"), quiet = TRUE)
-  # ps = tgver::explore_sf(nc, background = TRUE)
-  # expect_true(inherits(ps, "r_process"))
-  # ps$kill()
+
+  # from geojsonsf readme
+  js <- c(
+    '[
+      {"type":"Point","coordinates":[0,0]},
+      {"type":"LineString","coordinates":[[-1,-1],[1,1]]},
+        {
+      "type": "FeatureCollection",
+      "features": [
+      {
+        "type": "Feature",
+        "properties": {"id":1},
+        "geometry": {"type": "Point", "coordinates": [100.0, 0.0]}
+      }
+    ]
+  }
+    ]'
+  )
+
+  sf = geojsonsf::geojson_sf( js )
+  ps = tgver::explore_sf(sf, background = TRUE)
+  expect_true(inherits(ps, "r_process"))
+  ps$kill()
 })
