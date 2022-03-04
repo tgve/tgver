5ac365ca15d4fb2c306b3a0a3c469bb2b41e17cf
Layik Hama <layik.hama@gmail.com>
2022-03-04 10:17:44 +0000

    prep for merge

--- R/explore_sf.R
+++ R/explore_sf.R
@@ -1,20 +1,36 @@
-#' Explore an sf R object using TGVE.
-#'
+#' Explore an sf R object using TGVE npm package.
 #'
 #'
 #' @param sf a valid sf object that can be converted to geojson
-#' @param background boolean to decide whether plumber should run in the
-#' background
-#' @param static boolean to decide whether data is written to disk and self
-#' contained application is built
-#' @param path path of a TGVE instance, defaults to one in `tempdir()`
+#' @param background Boolean to decide whether plumber
+#' should run in the background
+#' @return depending on `background` either a or not
+#' blocking `plumber::pr` object is started or returned. In the case of a
+#' `backgruond` FALSE value a message is displayed with object details.
 #'
-#' @examples \dontrun{
-#' explore_sf()
+#' @examples {
+#' gj = c(
+#' '[
+#'       {"type":"Point","coordinates":[0,0]},
+#'       {"type":"LineString","coordinates":[[-1,-1],[1,1]]},
+#'         {
+#'       "type": "FeatureCollection",
+#'       "features": [
+#'       {
+#'         "type": "Feature",
+#'         "properties": {"id":1},
+#'         "geometry": {"type": "Point", "coordinates": [100.0, 0.0]}
+#'       }
+#'     ]
+#'   }
+#'     ]'
+#' )
+#' sf = geojsonsf::geojson_sf(gj)
+#' ps = tgver::explore_sf(sf, background = TRUE)
+#' ps$kill()
 #' }
 #' @export
-explore_sf = function(sf = NULL, background = FALSE, static = FALSE,
-                      path = tempInstance()) {
+explore_sf = function(sf = NULL, background = FALSE) {
   if(is.null(sf) || !inherits(sf, "sf")) {
     stop("Error: explore_sf requires an sf object.")
   }
@@ -22,27 +38,12 @@ explore_sf = function(sf = NULL, background = FALSE, static = FALSE,
   # data
   geojson = geojsonsf::sf_geojson(sf, simplify = FALSE, factors_as_string=FALSE)
 
-  # if writing data to index.html
-  if(static) {
-    html = file.path(path, "index.html")
-    # clean copy
-    file.copy(file.path(path, "index.original"), html, overwrite = TRUE)
-    # write data
-    file_replace(html, "</head>", paste0(
-      "<script id='tgve-data' type='application/json'>",
-      geojson, "</script></head>"
-    ))
-    message("Attempting to browse TGVE from: ", path)
-    openURL(html)
-    return(path)
-  } else {
-    # prepare back-end
-    endpoint = "/explore_sf"
-    explore_geojson(endpoint, geojson, background)
-  }
+  # prepare back-end
+  endpoint = "/explore_sf"
+  explore_geojson(endpoint, geojson, background)
 }
 
-explore_geojson = function(endpoint, geojson, background, path = tempInstance()) {
+explore_geojson = function(endpoint, geojson, background) {
   stopifnotonecharacter(endpoint)
   if(!is.character(geojson) || !is.atomic(geojson)) {
     stop("explore_geojsoin requires geoson object.")
@@ -53,6 +54,7 @@ explore_geojson = function(endpoint, geojson, background, path = tempInstance())
 
   base = "http://127.0.0.1:8000"
   endpoint.url = paste0(base, endpoint)
+  path = tempInstance()
   server = tgve_server(path = path, run = FALSE)
   # flexible variable names
   server$handle("GET", endpoint, function(res){
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,8 +2,13 @@ test_that("tempInstance works", {
   t = tempInstance()
   expect_true(dir.exists(t))
   expect_true(length(list.files(t, pattern = "html")) == 1)
-  expect_true(length(list.files(t, pattern = "original")) == 1)
-  expect_true(length(list.files(t, pattern = "index")) == 2)
+  # TODO check version against
+  # version = jsonlite::parse_json(
+  #   readLines(file.path(build.dir, "package.json")))$dependencies['@tgve/tgvejs'][[1]]
+  # version = sub(".", "", version)
+  # names(version) <- "version"
+  # How do we get the build version? It is not there by default
+  # clues for future https://stackoverflow.com/a/58421941/11101153
 })
 
 test_that("is_valid_url works", {
@@ -14,34 +19,34 @@ test_that("is_valid_url works", {
   expect_true(all(urls))
   # from https://cran.r-project.org/web/packages/rex/vignettes/url_parsing.html
   good = c("http://foo.com/blah_blah",
-            "http://foo.com/blah_blah/",
-            "http://foo.com/blah_blah_(wikipedia)",
-            "http://foo.com/blah_blah_(wikipedia)_(again)",
-            "http://www.example.com/wpstyle/?p=364",
-            "https://www.example.com/foo/?bar=baz&inga=42&quux",
-            "http://✪df.ws/123",
-            "http://userid:password@example.com:8080",
-            "http://userid:password@example.com:8080/",
-            "http://userid@example.com",
-            "http://userid@example.com/",
-            "http://userid@example.com:8080",
-            "http://userid@example.com:8080/",
-            "http://userid:password@example.com",
-            "http://userid:password@example.com/",
-            "http://foo.com/blah_(wikipedia)#cite-1",
-            "http://foo.com/blah_(wikipedia)_blah#cite-1",
-            "http://foo.com/unicode_(✪)_in_parens",
-            "http://foo.com/(something)?after=parens",
-            "http://☺.damowmow.com/",
-            "http://code.google.com/events/#&product=browser",
-            "http://j.mp",
-            "ftp://foo.bar/baz",
-            "http://foo.bar/?q=Test%20URL-encoded%20stuff",
-            "http://مثال.إختبار",
-            "http://例子.测试",
-            "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com",
-            "http://1337.net",
-            "http://a.b-c.de")
+           "http://foo.com/blah_blah/",
+           "http://foo.com/blah_blah_(wikipedia)",
+           "http://foo.com/blah_blah_(wikipedia)_(again)",
+           "http://www.example.com/wpstyle/?p=364",
+           "https://www.example.com/foo/?bar=baz&inga=42&quux",
+           "http://✪df.ws/123",
+           "http://userid:password@example.com:8080",
+           "http://userid:password@example.com:8080/",
+           "http://userid@example.com",
+           "http://userid@example.com/",
+           "http://userid@example.com:8080",
+           "http://userid@example.com:8080/",
+           "http://userid:password@example.com",
+           "http://userid:password@example.com/",
+           "http://foo.com/blah_(wikipedia)#cite-1",
+           "http://foo.com/blah_(wikipedia)_blah#cite-1",
+           "http://foo.com/unicode_(✪)_in_parens",
+           "http://foo.com/(something)?after=parens",
+           "http://☺.damowmow.com/",
+           "http://code.google.com/events/#&product=browser",
+           "http://j.mp",
+           "ftp://foo.bar/baz",
+           "http://foo.bar/?q=Test%20URL-encoded%20stuff",
+           "http://مثال.إختبار",
+           "http://例子.测试",
+           "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com",
+           "http://1337.net",
+           "http://a.b-c.de")
   expect_true(all(is_valid_url(good)))
   bad =  c(
     "http://",
